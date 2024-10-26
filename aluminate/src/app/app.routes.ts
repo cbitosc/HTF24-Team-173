ed25020cf5d9ef441a9471f87af9c8461226f4a1
Sahith-Rao <sahithraolingampally@gmail.com>
2024-10-27 04:18:30 +0530

    basic styling for events page

--- aluminate/src/app/app.routes.ts
+++ aluminate/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component'; 
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { EventsPageComponent } from './components/events-page/events-page.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +17,13 @@ export const routes: Routes = [
     {
         path: '',
         component: LandingPageComponent
+    },
+    {
+        path: 'home',
+        component: HomePageComponent
+    },
+    {
+        path: 'events',
+        component: EventsPageComponent
     }
 ];
