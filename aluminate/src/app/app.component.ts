33791cd1f12a9e1b8c9903758917d80a0f0872db
Karthik-18-02 <karthiksamala33@gmail.com>
2024-10-27 03:10:03 +0530

    basic home page

--- aluminate/src/app/app.component.ts
+++ aluminate/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, LoginComponent],
+  imports: [RouterOutlet, LandingPageComponent, LoginComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
