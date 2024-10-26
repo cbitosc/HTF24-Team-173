97b4658d7edfe23def01a85745d4b487ab4c1bef
Karthik-18-02 <karthiksamala33@gmail.com>
2024-10-27 03:13:52 +0530

    basic home page 2

--- aluminate/src/app/app.routes.ts
+++ aluminate/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component'; 
 
 export const routes: Routes = [
     {
---
+++ aluminate/src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+}
