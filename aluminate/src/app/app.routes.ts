a26eba07fe56114f7135d249a25d356e3aca342a
Sahith-Rao <sahithraolingampally@gmail.com>
2024-10-27 02:26:49 +0530

    login and register routes added

--- aluminate/src/app/app.routes.ts
+++ aluminate/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
     },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: '',
+        component: LandingPageComponent
+    }
 ];
