106a7e89b9f5862e958348bc5c636f735fc808d3
Karthik-18-02 <karthiksamala33@gmail.com>
2024-10-27 10:46:02 +0530

    pulled karthik

--- aluminate/src/app/app.component.ts
+++ aluminate/src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { EventsPageComponent } from './components/events-page/events-page.component';
+import { DonationsPageComponent } from './components/donations-page/donations-page.component';
+import { RegisterComponent } from './components/register/register.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, LoginComponent, HomePageComponent],
+  imports: [RouterOutlet, RegisterComponent, DonationsPageComponent, EventsPageComponent, LandingPageComponent, LoginComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
